f91cd5ff42e291a4cf7689a6e428ec3511c37f34
Dan Rogers <dan@danro.net>
2013-12-13 14:23:07 -0800

    Fix indent in jQuery file

--- jquery.tap.js
+++ jquery.tap.js
@@ -3,33 +3,33 @@
  * Released under MIT license
  */
 (function ($) {
-  'use strict';
-  
-  $.event.special.tap = (function () {
+    'use strict';
     
-    // Fallback to click events in old IE
-    if (!document.addEventListener) return { bindType: 'click', delegateType: 'click' };
-    
-    var Tap = window.Tap;
-    var dataKey = 'tap.js';
-    
-    return {
-      setup: function () {
-        $.data(this, dataKey, new Tap(this));
-      },
-      teardown: function () {
-        var tap = $.data(this, dataKey);
-        if (tap && tap.destroy) {
-          tap.destroy();
-          $.removeData(this, dataKey);
-        }
-      },
-      add: function (handleObj) {
-        this.addEventListener('tap', handleObj.handler, false);
-      },
-      remove: function (handleObj) {
-        this.removeEventListener('tap', handleObj.handler, false);
-      }
-    };
-  }());
+    $.event.special.tap = (function () {
+        
+        // Fallback to click events in old IE
+        if (!document.addEventListener) return { bindType: 'click', delegateType: 'click' };
+        
+        var Tap = window.Tap;
+        var dataKey = 'tap.js';
+        
+        return {
+            setup: function () {
+                $.data(this, dataKey, new Tap(this));
+            },
+            teardown: function () {
+                var tap = $.data(this, dataKey);
+                if (tap && tap.destroy) {
+                    tap.destroy();
+                    $.removeData(this, dataKey);
+                }
+            },
+            add: function (handleObj) {
+                this.addEventListener('tap', handleObj.handler, false);
+            },
+            remove: function (handleObj) {
+                this.removeEventListener('tap', handleObj.handler, false);
+            }
+        };
+    }());
 }(window.jQuery));
